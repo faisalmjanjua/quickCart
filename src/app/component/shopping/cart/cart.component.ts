156c6d42277c31cf90478858855da22a58355d4d
faisalmjanjua <fjanroid@gmail.com>
2023-04-25 11:06:52 +0500

    update cart

--- src/app/component/shopping/cart/cart.component.ts
+++ src/app/component/shopping/cart/cart.component.ts
@@ -12,16 +12,52 @@ export class CartComponent implements OnInit {
   productItem: Products[] = [];
   del = faTimes;
 
+  subTotal = 0;
+  grantTotal = 0;
+  totalQuantity = 0;
+  deliveryCharges: number = 5;
+  discount = 0;
+  totalDiscount = 0;
+
   constructor(private cartSrv: CartService) {}
 
   ngOnInit(): void {
-    this.getProduct();
+    this.cartSrv.getProduct().subscribe((item: any) => {
+      this.getProduct(item);
+    });
   }
-  
-  getProduct() {
-    this.cartSrv.getProduct().subscribe((x: any) => {
-      this.productItem.push(x);
-      console.log(this.productItem, 'cartss');
+
+  getProduct(item) {
+    let itemExists = false;
+
+    for (let i in this.productItem) {
+      if (this.productItem[i].id === item.id) {
+        this.productItem[i].qty++;
+        itemExists = true;
+        break;
+      }
+    }
+
+    if (!itemExists) {
+      this.productItem.push(item);
+    }
+
+    this.subTotal = 0;
+    this.grantTotal = 0;
+    this.discount = 0;
+    this.totalDiscount = 0;
+    this.totalQuantity = 0;
+
+    this.productItem.forEach((element) => {
+      this.totalQuantity += element.qty;
+      this.subTotal += element.qty * element.price;
+
+      if (this.totalQuantity > 2) {
+        this.discount = 3;
+        this.totalDiscount = (this.discount / 100) * this.subTotal;
+      }
+      this.grantTotal =
+        this.subTotal - this.totalDiscount + this.deliveryCharges;
     });
   }
 }
--- src/app/shared/products.service.ts
+++ src/app/shared/products.service.ts
@@ -14,30 +14,96 @@ export class ProductsService {
       'plastic',
       52.99,
       0,
-      10,
-      'assets/prod1.png'
+      1,
+      'assets/cedarwood.png'
     ),
     new Products(
-      1,
+      2,
       'Lavender',
       'Lavender essential oil is perfect for your bath or shower',
       5,
       'plastic',
       52.99,
       0,
-      10,
-      'assets/prod1.png'
+      1,
+      'assets/lavender.png'
     ),
     new Products(
-      1,
+      3,
       'Vetiver',
       'Effect on both the mind and the skin when used in a aromatherpy',
       5,
       'plastic',
       52.99,
       0,
-      10,
-      'assets/prod1.png'
+      1,
+      'assets/vetiver.png'
+    ),
+    new Products(
+      4,
+      'Jasmine',
+      'Jasmine essential oil has been used for centuries in the form of perfume ',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/jasmine.png'
+    ),
+    new Products(
+      5,
+      'Gardenia',
+      'Gardenia essential oil is primarily used as fragrance.',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/gardenia.png'
+    ),
+    new Products(
+      6,
+      'Chamonile',
+      'THe chamomile roman essential oil also referred to as Roman.',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/chamonile.png'
+    ),
+    new Products(
+      7,
+      'Peppermin',
+      'Peppermint essential oil is known for its ability to calm the mind, body.',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/peppermin.png'
+    ),
+    new Products(
+      8,
+      'Lemon',
+      'The citrus lemon essential oil has a myriad of uses. It can be used.',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/lemon.png'
+    ),
+    new Products(
+      9,
+      'Bergamot',
+      'The oil obtained from its peel gives it a fresh aroma with hints of grapefruit.',
+      5,
+      'plastic',
+      52.99,
+      0,
+      1,
+      'assets/bergamot.png'
     ),
   ];
 
